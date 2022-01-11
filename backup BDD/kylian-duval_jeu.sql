93c613c6be299aa42ad70275ba7686825ee6e909
kylian-duval <kduval@la-providence.net>
2022-01-11 13:28:07 +0100

    update

--- backup BDD/kylian-duval_jeu.sql
+++ backup BDD/kylian-duval_jeu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-kylian-duval.alwaysdata.net
--- Generation Time: Jan 11, 2022 at 11:26 AM
+-- Generation Time: Jan 11, 2022 at 01:26 PM
 -- Server version: 10.5.11-MariaDB
 -- PHP Version: 7.4.19
 
@@ -20,6 +20,44 @@ SET time_zone = "+00:00";
 --
 -- Database: `kylian-duval_jeu`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id_ commentaire` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_jeu` int(11) NOT NULL,
+  `Message` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `commentaire`
+--
+
+INSERT INTO `commentaire` (`id_ commentaire`, `id_user`, `id_jeu`, `Message`) VALUES
+(16, 13, 1, 'super'),
+(23, 16, 4, 'COucou'),
+(25, 17, 1, 'Test'),
+(26, 18, 7, 'Bon jeu à faire entre potes '),
+(27, 18, 10, 'Bon jeu quand il bug pas il te faut une config monstre pour éviter les bugs mais bon jeu ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_message` int(11) NOT NULL,
+  `Nom` varchar(50) NOT NULL,
+  `Prénom` varchar(50) NOT NULL,
+  `Mail` varchar(50) NOT NULL,
+  `Message` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,11 +87,73 @@ INSERT INTO `jeux` (`id_jeux`, `Nom`, `Image`, `Image2`, `Image3`, `Image4`, `De
 (6, 'Tom clancy\'s rainbow six siege', 'rainbow.png', 'image_cover.jpg', '12-1-2015_14_1_1.jpg', 'tom-clancys-rainbow-sixr-siege2017-10-16-16-16-9.jpg', 'Rainbow Six: Siege est un jeu de tir tactique où le joueur incarne différents agents de plusieurs unités de forces spéciales et de groupes d\'interventions qui constituent l’équipe Rainbow. Comme les autres titres de la série, il se concentre fortement sur le jeu en équipe et le \"réalisme\" des interventions. Cependant, il existe de grandes différences par rapport à d\'anciennes versions du jeu, avec un accent multijoueur important et des environnements entièrement destructibles. En tout, 10 cartes, le SWAT américain, le SAS anglais, le GSG 9 allemand, les Spetsnaz russes et le GIGN français sont disponibles lors de la sortie du jeu. Actuellement, 24 unités d\'interventions et 21 cartes sont disponibles dans Tom Clancy’s Rainbow Six: Siege.'),
 (7, 'Grand Theft Auto V', 'gta.png', 'GTA-V-big-890x501.jpg', '801x410_d56be-593356900.jpg', '200.gif', 'Grand Theft Auto V (plus communément abrégé GTA V) est un jeu vidéo d\'action-aventure, développé par Rockstar North et édité par Rockstar Games. Il est sorti en 2013 sur PlayStation 3 et Xbox 360, en 2014 sur PlayStation 4 et Xbox One puis en 2015 sur PC. Le jeu fait partie de la série vidéoludique Grand Theft Auto.\r\n\r\nCet épisode se déroule dans l\'État fictif de San Andreas en Californie du Sud. L\'histoire solo suit trois protagonistes?: le braqueur de banque à la retraite Michael De Santa, le gangster Franklin Clinton et le trafiquant de drogue et d\'armes Trevor Philips, et leurs braquages sous la pression d\'une agence gouvernementale corrompue et de puissants criminels. Le jeu en monde ouvert permet aux joueurs de parcourir librement la campagne ouverte de San Andreas et la ville fictive de Los Santos, basée sur Los Angeles.'),
 (9, 'hitman 3', 'hitman3.png', 'B9725927528Z.1_20210125174318_000+G6HHFM919.2-0.jpg', 'Landscape-2560x1440-bd7696342eacc951048c0765a204f371.jpg', '1597741042-9381-card.jpg', 'Hitman 3 est un jeu vidéo d\'action-aventure et d\'infiltration développé et édité par IO Interactive, sorti le 20 janvier 2021 sur PC, PlayStation 4, PlayStation 5, Xbox One, Xbox Series. Le jeu est également jouable sur Stadia et Nintendo Switch via le cloud gaming.\r\n\r\nTroisième et dernière partie de la trilogie World of Assassination, il fait suite aux jeux Hitman (2016) et Hitman 2 (2018). Les développeurs ont déclaré que le jeu sera « beaucoup plus mature, sérieux et sombre » que les précédents épisodes de la trilogie.'),
-(10, 'Cyberpunk 2077', 'CYBERPUNK-2077.png', 'raw.jpg', 'cyberpunk2077.jpg', 'Cyberpunk_2077_website_1617083795450.jpg.jpeg', 'Cyberpunk 2077 est un jeu vidéo d\'action-RPG en vue à la première personne développé par CD Projekt RED, inspiré du jeu de rôle sur table Cyberpunk 2020 conçue par Mike Pondsmith. Le jeu présente un monde futuriste dystopique de type cyberpunk dans lequel la technologie coexiste avec une société humaine dégénérée');
+(10, 'Cyberpunk 2077', 'EEA9B8CB-8E22-418B-B310-A2080E8D6AD9.png', 'A364AD74-EECD-4EF2-B681-8505DF33484A.jpeg', 'AFDC3554-B648-4AC5-ADC5-450A8A334126.jpeg', 'ECA36327-247B-4368-B628-EAD15A28285D.jpeg', 'Cyberpunk 2077 est un jeu vidéo d\'action-RPG en vue à la première personne développé par CD Projekt RED, inspiré du jeu de rôle sur table Cyberpunk 2020 conçue par Mike Pondsmith. Le jeu présente un monde futuriste dystopique de type cyberpunk dans lequel la technologie coexiste avec une société humaine dégénérée');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `note`
+--
+
+CREATE TABLE `note` (
+  `id_note` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_jeu` int(11) NOT NULL,
+  `note` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `note`
+--
+
+INSERT INTO `note` (`id_note`, `id_user`, `id_jeu`, `note`) VALUES
+(4, 13, 1, 5),
+(7, 16, 4, 1),
+(9, 17, 1, 5),
+(10, 18, 7, 4),
+(12, 18, 10, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `Nom_user` varchar(30) NOT NULL,
+  `Mdp` varchar(30) NOT NULL,
+  `Admin` varchar(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `Nom_user`, `Mdp`, `Admin`) VALUES
+(9, 'duval ', '123', 'FALSE'),
+(10, 'admin ', '123', 'FALSE'),
+(12, 'kylian ', '123', 'FALSE'),
+(13, 'Louis ', 'motdepassesecure', 'FALSE'),
+(16, 'Rapidecho ', 'sUjbiv-wunjiq-4cetry', 'FALSE'),
+(17, 'Gaëlle ', 'Espoirjoliat4', 'FALSE'),
+(18, 'Shitorama ', '1111', 'FALSE');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id_ commentaire`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_message`);
 
 --
 -- Indexes for table `jeux`
@@ -63,14 +163,50 @@ ALTER TABLE `jeux`
 ALTER TABLE `jeux` ADD FULLTEXT KEY `Description` (`Description`);
 
 --
+-- Indexes for table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`id_note`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `commentaire`
+--
+ALTER TABLE `commentaire`
+  MODIFY `id_ commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jeux`
 --
 ALTER TABLE `jeux`
   MODIFY `id_jeux` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `note`
+--
+ALTER TABLE `note`
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
