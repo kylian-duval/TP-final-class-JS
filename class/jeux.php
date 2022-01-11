f51fa38848bd6426f94e030f8a253a6203b77673
kylian-duval <kduval@la-providence.net>
2022-01-11 17:44:45 +0100

    Update jeux.php

---
+++ class/jeux.php
@@ -0,0 +1,39 @@
+<?php
+
+class jeux
+{
+    private $_id;
+    private $_nom;
+    private $_image1;
+    private $_image2;
+    private $_image3;
+    private $_image4;
+    private $_description;
+    private $_BDD;
+
+    public function __construct($BDD)
+    {
+        $this->_BDD = $BDD;
+    }
+
+    public function ajoutjeux($nom, $image1, $image2, $image3, $image4, $description)
+    {
+        $nom = htmlspecialchars($nom);
+        $image1 = htmlspecialchars($image1);
+        $image2 = htmlspecialchars($image2);
+        $image3 = htmlspecialchars($image3);
+        $image4 = htmlspecialchars($image4);
+        $description = htmlspecialchars($description);
+    }
+
+    public function getjeux($id)
+    {
+    }
+
+    public function removejeux($id){
+        unlink('images/$this->_image1');
+        unlink('images/$this->_image2');
+        unlink('images/$this->_image3');
+        unlink('images/$this->_image4');
+    }
+}
